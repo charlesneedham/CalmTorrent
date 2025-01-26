c2a2a57a82be68f4f3f9e06a5fe9f05193095145
charles.needham <charles.needham@agfa.com>
2025-01-26 18:08:46 +0000

    yeah

--- flask-app/__init__.py
+++ flask-app/__init__.py
@@ -1,45 +1,12 @@
 from flask import Flask, render_template, redirect, url_for, request, send_from_directory
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import os
 
-login_manager = LoginManager()
-
-class User(UserMixin):
-        def __init__(self, id):
-            self.id = id
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your_secret_key'
     
-    login_manager.init_app(app)
-    login_manager.login_view = 'login'
-
-    users = {'user': {'password': 'password'}}
-
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User(user_id)
-
-    @app.route('/login', methods=['GET', 'POST'])
-    def login():
-        if request.method == 'POST':
-            username = request.form['username']
-            password = request.form['password']
-            if username in users and users[username]['password'] == password:
-                user = User(username)
-                login_user(user)
-                return redirect(url_for('rss_feed'))
-        return render_template('login.html')
-
-    @app.route('/logout')
-    @login_required
-    def logout():
-        logout_user()
-        return redirect(url_for('login'))
-
     @app.route('/rss_feed')
-    @login_required
+
     def rss_feed():
         rss_directory = os.path.join(app.root_path, 'rss_xml')
         return send_from_directory(rss_directory, 'feed.xml')
