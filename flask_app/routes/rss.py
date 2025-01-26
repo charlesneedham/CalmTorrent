bd41ae44d2e3d2bf7c4158ca5385932ddd150203
charles.needham <charles.needham@agfa.com>
2025-01-26 18:22:07 +0000

    yeah

--- flask_app/routes/rss.py
+++ flask_app/routes/rss.py
@@ -1,11 +1,10 @@
-from flask import send_from_directory, Blueprint, Flask
+from flask import Blueprint, send_file, Flask
 import os
 
 app = Flask(__name__)
 bp = Blueprint('rss', __name__)
 
-
-@bp.route('/rss_feed', methods=['GET'])
+@bp.route('/rss_feed')
 def rss_feed():
-    rss_directory = os.path.join(app.root_path, 'rss_xml')
-    return send_from_directory(rss_directory, 'movies.xml')
+    rss_file_path = os.path.join(os.path.dirname(__file__), '../../rss_xml/movies.xml')
+    return send_file(rss_file_path, mimetype='application/rss+xml')
